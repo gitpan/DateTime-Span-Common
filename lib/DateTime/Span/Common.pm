585f0a0f34ebda951738536af593a6ce4328d5ff
Terrence Brannon <metaperl@gmail.com>
2009-08-06 19:43:51 +0000

    Import of TBONE/DateTime-Span-Common-0.03 from CPAN.
    gitpan-cpan-distribution: DateTime-Span-Common
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         TBONE/DateTime-Span-Common-0.03.tar.gz
    gitpan-cpan-author:       TBONE
    gitpan-cpan-maturity:     released

--- lib/DateTime/Span/Common.pm
+++ lib/DateTime/Span/Common.pm
@@ -1,5 +1,7 @@
 package DateTime::Span::Common;
 
+our $VERSION = '0.03';
+
 use Moose;
 use Moose::Util::TypeConstraints;
 
@@ -25,7 +27,7 @@ sub set_eod {
 sub DateTime::nowe {
 
     my $now = DateTime->now;
-    $now->set_eod;
+    set_eod(undef, $now);
 
 }
 
@@ -38,7 +40,7 @@ sub DateTime::Span::datetimes {
 sub DateTime::Span::from_array {
     my ($self, $start,$end)=@_;
 
-    warn "$start , $end";
+    #warn "$start , $end";
 
     DateTime::Span->from_datetimes( start => $start, end => $end ) ;
 }
@@ -128,164 +130,5 @@ sub last_year {
     DateTime::Span->from_datetimes( start => $soy, end => $eoy ) ;
 }
 
-
-
-
-
-=head1 NAME
-
-DateTime::Span::Common - create common DateTime::Span instances
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
-
-=head1 SYNOPSIS
-
-    use DateTime::Span::Common;
-
-    my $x = DateTime::Span::Common->new;
-
-
-    my $dts = $x->today;
-    $dts = $x->yesterday;
-    $dts = $x->this_week;
-    $dts = $x->last_week;
-    $dts = $x->this_month;
-    $dts = $x->last_month;
-    $dts = $x->year_to_date;
-    $dts = $x->last_year;
-
-    my $sth = $dbh->prepare('SELECT * FROM table WHERE order_date BETWEEN ? AND ?');
-    $sth->execute($dts->datetimes);
-
-=head1 DESCRIPTION
-
-DateTime::Span::Common is a module which provides a L<DateTime::Span|DateTime::Span> object for a 
-number of common date spans. It also provides a few convenience methods which I found useful in 
-developing this module.
-
-=head1 METHODS
-
-=head2 new
-
-When constructing a DateTime::Span::Common object, you may change the day on which the week is 
-presumed to start from Sunday to any other day in the week:
-
-   my $dtsc = DateTime::Span::Common->new(week_start_day => 'Monday');
-
-=head2 today
-
-Returns a L<DateTime::Span> object covering the current day.
-
-=head2 yesterday
-
-Returns a L<DateTime::Span> object covering yesterday.
-
-=head2 this_week
-
-Returns a L<DateTime::Span> object covering this week, respective of C<< $self->week_start_day >> .
-
-=head2 last_week
-
-Returns a L<DateTime::Span> object covering last week, respective of C<< $self->week_start_day >> .
-
-=head2 this_month
-
-Returns a L<DateTime::Span> object covering this month to date.
-
-=head2 this_year
-
-Returns a L<DateTime::Span> object covering this year to date.
-
-=head2 last_year
-
-Returns a L<DateTime::Span> object covering last year to date.
-
-=head1 Internal Methods
-
-=head2 set_eod
-
-This method is used for end of day calculations. It sets a L<DateTime|DateTime> object's day, minute, and second 
-fields by default. You can override it for your own purposes in your subclass as necessary.
-
-=head2 DateTime::Span::datetimes
-
-The public methods of this module return a L<Datetime::Span> instance. Oftentimes you will want the start and end of the span 
-in an array. This method gives it to you:
-
-   my $datetime_span = DateTime::Span::Common->new->this_year;
-   my $sth = $dbh->prepare('SELECT * FROM table WHERE order_date BETWEEN ? AND ?');
-   $sth->execute($datetime_span->datetimes);
-
-=head2 DateTime::Span::from_array
-
-This method creates a L<DateTime::Span> instance from an array:
-
-  my @data = some_api_result();
-  my $dts = DateTime::Span->from_array(@data);
-
-
-
-=head1 AUTHOR
-
-Terrence Brannon, C<< <metaperl at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-datetime-span-common at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=DateTime-Span-Common>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc DateTime::Span::Common
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=DateTime-Span-Common>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/DateTime-Span-Common>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/DateTime-Span-Common>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/DateTime-Span-Common/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 Terrence Brannon, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-
-=cut
 
 1; # End of DateTime::Span::Common
